af703988f432faa873ad66f146423b7a4d1d17ce
iecbossteam1cfgsl <iecbossteam1cfgsl@gmail.com>
2018-04-25 20:50:26 +0200

    check methods

--- JavaWorkShopGitProject/src/main/workshopgitproject/tests/TestInvoiceLine.java
+++ JavaWorkShopGitProject/src/main/workshopgitproject/tests/TestInvoiceLine.java
@@ -3,6 +3,10 @@
  *******************************************************************************/
 package main.workshopgitproject.tests;
 
+import main.workshopgitproject.classes.Invoice;
+import main.workshopgitproject.classes.InvoiceLine;
+import main.workshopgitproject.classes.Product;
+
 // Start of user code (user defined imports)
 
 // End of user code
@@ -29,10 +33,26 @@ public class TestInvoiceLine {
 	/**
 	 * Description of the method testInvoiceLineAmount.
 	 */
+	
 	public void testInvoiceLineAmount() {
 		// Start of user code for method testInvoiceLineAmount
 		// End of user code
-		//
+		//Vamos a calcular el importe de 4 paquetes de galletas,
+		//sabiendo que cada paquete vale 2,5 euros
+		//Create a new product
+		Product p = new Product();
+		p.setIdentifier(1);
+		p.setDescription("Paquete de galletas");
+		p.setPrice(2.5);
+		p.setStock(14);
+		//Create a new InvoiceLine
+		InvoiceLine ivn= new InvoiceLine();
+		ivn.setNumber(1);
+		ivn.setProducts(p);
+		ivn.setQuantity(4);
+		
+		double correctPrice=10;
+		assertEquals(correctPrice,ivn.getInvoiceLineAmount());
 	}
 
 	/**
@@ -41,9 +61,35 @@ public class TestInvoiceLine {
 	public void testInvoiceAmount() {
 		// Start of user code for method testInvoiceAmount
 		// End of user code
-		//Vamos a calcular el importe de 4 paquetes de galletas,
-		//sabiendo que cada paquete vale 2,5 euros
-		//Assert.Equals(4,l.calculate..clone().)
+		Product p = new Product();
+		p.setIdentifier(1);
+		p.setDescription("Cockie box");
+		p.setPrice(2.5);
+		p.setStock(14);
+		//Create a new InvoiceLine
+		InvoiceLine ivn= new InvoiceLine();
+		ivn.setNumber(1);
+		ivn.setProducts(p);
+		ivn.setQuantity(4);
+		//Add another product
+		Product p1 = new Product();
+		p1.setIdentifier(2);
+		p1.setDescription("Razors packet");
+		p1.setPrice(6);
+		p1.setStock(14);
+		//Create a new InvoiceLine
+		InvoiceLine ivn1= new InvoiceLine();
+		ivn1.setNumber(2);
+		ivn1.setProducts(p1);
+		ivn1.setQuantity(5);
+		//Create a new Invoice
+		//Total amount should be 10€+30€
+		Invoice invo= new Invoice();
+		
+		double correctInvoiceAmount=40;
+		assertEquals(correctInvoiceAmount,invo.getTotalAmount());
+		
+		
 	}
 
 	/**
@@ -52,6 +98,13 @@ public class TestInvoiceLine {
 	public void testAvaliableStock() {
 		// Start of user code for method testAvaliableStock
 		// End of user code
+		Product p = new Product();
+		p.setIdentifier(1);
+		p.setDescription("Cockie box");
+		p.setPrice(2.5);
+		p.setStock(14);
+		
+		assertTrue(p.getStock()>0);
 	}
 
 	// Start of user code (user defined methods for TestInvoiceLine)
